6ad4e3dcf62ab193fc931be3a5e2002ee1d27650
Joshua Goh <74043789+goshuajoh@users.noreply.github.com>
2024-09-17 00:08:03 +0800

    Update Lab2Task5.ino

--- Lab2Task5.ino
+++ Lab2Task5.ino
@@ -1,99 +1,74 @@
 #include <Arduino.h>
-#include <SparkFun_APDS9960.h>
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <HTTPClient.h>
-
-#define USE_SERIAL Serial
+#include "DHT.h"  //Use DHT sensor library from Lab 1
 
 WiFiMulti wifiMulti;
 
-SparkFun_APDS9960 apds = SparkFun_APDS9960();
-const char* serverName = "http://192.168.0.1:3237/data"; // Use your computer's IP address
+const char* ssid = "SSID";
+const char* password = "Password!";
+
+const char* laptopAt = "Ipaddress"; //Laptop IP address
+
+// DHT sensor setup
+#define DHTPIN 4     //conneted to GPIO pin 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  USE_SERIAL.begin(115200);
 
-  // Connect to WiFi
-  wifiMulti.addAP("25LentorGoh", "Oct0b3r10!");
+    Serial.begin(115200);
+    dht.begin();
 
-  // Wait for connection
-  while (wifiMulti.run() != WL_CONNECTED) {
-    delay(1000);
-    USE_SERIAL.println("Connecting to WiFi...");
-  }
+    wifiMulti.addAP(ssid, password);
 
-  USE_SERIAL.println("Connected to WiFi!");
-
-  // Initialize the gesture sensor
-  if (apds.enableGestureSensor(true)) {
-    Serial.println("Gesture sensor is now running");
-  } else {
-    Serial.println("Something went wrong during gesture sensor init!");
-  }
+    while (wifiMulti.run() != WL_CONNECTED) {
+      delay(500);
+    }
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  String gestureData = "";
-  
-  if (apds.isGestureAvailable()) {
-    switch (apds.readGesture()) {
-      case DIR_UP:
-        Serial.println("UP gesture detected");
-        gestureData = "UP";
-        break;
-      case DIR_DOWN:
-        Serial.println("DOWN gesture detected");
-        gestureData = "DOWN";
-        break;
-      case DIR_LEFT:
-        Serial.println("LEFT gesture detected");
-        gestureData = "LEFT";
-        break;
-      case DIR_RIGHT:
-        Serial.println("RIGHT gesture detected");
-        gestureData = "RIGHT";
-        break;
-      case DIR_NEAR:
-        Serial.println("NEAR gesture detected");
-        gestureData = "NEAR";
-        break;
-      case DIR_FAR:
-        Serial.println("FAR gesture detected");
-        gestureData = "FAR";
-        break;
-      default:
-        Serial.println("Unknown gesture");
-        gestureData = "UNKNOWN";
-        break;
+  if (WiFi.status() == WL_CONNECTED){
+    Serial.println("Sending...");
+
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+
+    Serial.print("Raw temperature: ");
+    Serial.println(temperature);
+    Serial.print("Raw humidity: ");
+    Serial.println(humidity);
+
+    if (isnan(temperature) || isnan(humidity)) {
+      Serial.println("Failed to read!");
+      return;
     }
 
-    // Send gesture data to Flask server
-    sendGestureData(gestureData);
-  }
+    int temperatureInt = (int)temperature;
+    int humidityInt = (int)humidity;
 
-  delay(5000);  // Wait for 5 seconds before checking again
-}
-
-void sendGestureData(String gesture) {
-  if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
 
-    // Build the request URL
-    String serverPath = String(serverName) + "?gesture=" + gesture;
+    String url = laptopAt;
+    url += "/data?data1=" + String(temperatureInt) + "&data2=" + String(humidityInt);
 
-    http.begin(serverPath.c_str());
-    int httpResponseCode = http.GET();
+    http.begin(url);
+    int returnCode = http.GET();   //Perform GET request
 
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println("Server Response: " + response);
-    } else {
-      Serial.println("Error in sending GET request");
+    if (returnCode > 0){
+      String payload = http.getString();
+      Serial.println(payload);
     }
-
     http.end();
+
   } else {
-    Serial.println("WiFi Disconnected");
+    Serial.println("WiFi disconnected");
   }
+  
+  delay(5000);
 }
